bcb47d581820301314e2235d830f9e2a51823999
Himasha3 <mashigayathri7@gmail.com>
2024-10-20 10:34:02 +0530

    @BeforeTest,@AfterTest

---
+++ src/test/java/day1/pack/C2.java
@@ -0,0 +1,23 @@
+package day1.pack;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class C2 {
+    
+	@Test
+	void xyz()
+	{
+		System.out.println("this is xyz from c2....");
+		
+	}
+	
+	@AfterTest
+	void at()
+	{
+		System.out.println("this is AfterTest method....");
+		
+	}
+	
+	
+}
